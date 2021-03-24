310251981eaecff41e80644bb79823890cbe90e2
sheliming <shelimingming@qq.com>
2021-03-24 15:53:18 +0800

    add log

--- src/main/java/com/sheliming/controller/HelloController.java
+++ src/main/java/com/sheliming/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.sheliming.controller;
 
+import com.sheliming.DemoApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
     @GetMapping("hello")
     public String hello(String name) {
-        return "hello "+name;
+        logger.info("hello " + name);
+        return "hello " + name;
     }
 }
